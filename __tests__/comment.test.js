110ef7b9afd3209ce00988026d738e0a829fa785
Krisna Lavendra <krisnalavendrairawan@gmail.com>
2023-06-02 19:53:38 +0700

    add edit comment test

--- __tests__/comment.test.js
+++ __tests__/comment.test.js
@@ -200,3 +200,85 @@ describe('GET /comments', () => {
         expect(typeof res.body).toEqual('object')
     })
 })
+
+describe('PUT /comments/:id', () => {
+    afterAll(async () => {
+        try {
+            await User.destroy({ where: {} })
+            await Photo.destroy({ where: {} })
+            await Comment.destroy({ where: {} })
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    beforeAll(async () => {
+        try{
+            await createUser()
+            await createUser2()
+            await createPhoto()
+            await createComment()
+            await createComment2()
+        }catch{
+            console.log(error);
+        }
+    })
+    //success response
+    it('should send response with 200 status code', async () => {
+        const res = await request(app)
+            .put('/comments/1')
+            .set('token', auth_token)
+            .send({comment: 'test'})
+        expect(res.statusCode).toEqual(201)
+        expect(typeof res.body).toEqual('object')
+        expect(res.body.comment[0]).toHaveProperty('id')
+        expect(res.body.comment[0]).toHaveProperty('comment')
+        expect(res.body.comment[0]).toHaveProperty('PhotoId')
+        expect(res.body.comment[0]).toHaveProperty('UserId')
+    })
+    //error response (no token)
+    it('should send response with 401 status code', async () => {
+        const res = await request(app)
+            .put('/comments/1')
+            .send({comment: 'test'})
+        expect(res.statusCode).toEqual(401)
+        expect(res.body).toHaveProperty('message', 'jwt must be provided')
+        expect(res.body).toHaveProperty('name', 'JsonWebTokenError')
+        expect(res.body).not.toHaveProperty('comment')
+        expect(res.body).not.toHaveProperty('id')
+        expect(res.body).not.toHaveProperty('PhotoId')
+        expect(res.body).not.toHaveProperty('UserId')
+        expect(typeof res.body).toEqual('object')
+    })
+
+    //error response (not authorized)
+    it('should send response with 401 status code', async () => {
+        const res = await request(app)
+            .put('/comments/2')
+            .set('token', auth_token)
+            .send({comment: 'test'})
+        expect(res.statusCode).toEqual(401)
+        expect(res.body).toHaveProperty('message', 'User not authorized')
+        expect(res.body).toHaveProperty("devMessage", "User with id 1 not authorized to id 2")
+        expect(res.body).not.toHaveProperty('comment')
+        expect(res.body).not.toHaveProperty('id')
+        expect(res.body).not.toHaveProperty('PhotoId')
+        expect(res.body).not.toHaveProperty('UserId')
+        expect(typeof res.body).toEqual('object')
+
+    })
+    //error response (comment not found)
+    it('should send response with 404 status code', async () => {
+        const res = await request(app)
+            .put('/comments/100')
+            .set('token', auth_token)
+            .send({comment: 'test'})
+        expect(res.statusCode).toEqual(404)
+        expect(res.body).toHaveProperty('message', 'Comment not found')
+        expect(res.body).toHaveProperty("devMessage", "Comment with id 100 not found")
+        expect(res.body).not.toHaveProperty('comment')
+        expect(res.body).not.toHaveProperty('id')
+        expect(res.body).not.toHaveProperty('PhotoId')
+        expect(res.body).not.toHaveProperty('UserId')
+        expect(typeof res.body).toEqual('object')
+    })
+})
